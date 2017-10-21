bb34579cda8e522edc77e68dd83dce3838c38c2f
bingweibi <794654932@qq.com>
2017-10-21 15:54:41 +0800

    1021顶部导航栏部分实现

--- app/src/main/java/com/example/bbw/openzz/MainActivity.java
+++ app/src/main/java/com/example/bbw/openzz/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.bbw.openzz;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.bbw.openzz.adapter.FragmentOneAdapter;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private TabLayout mTabBottomLayout;
+    private ViewPager mViewPager;
+    private FragmentOneAdapter mFragmentOneAdapter;
+
+    private TabLayout.Tab mTabOne;
+    private TabLayout.Tab mTabTwo;
+    private TabLayout.Tab mTabThree;
+    private TabLayout.Tab mTabFour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //初始化视图
+        initViews();
+    }
+
+    private void initViews() {
+
+        //使用适配器蒋ViewPager与Fragment绑定在一起
+        mViewPager = findViewById(R.id.viewPager);
+        mFragmentOneAdapter = new FragmentOneAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mFragmentOneAdapter);
+
+        //将TabLayout与ViewPager绑定在一起
+        mTabLayout = findViewById(R.id.tabLayout);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        //指定Tab的位置
+        mTabOne = mTabLayout.getTabAt(0);
+        mTabTwo = mTabLayout.getTabAt(1);
+        mTabThree = mTabLayout.getTabAt(2);
+        mTabFour = mTabLayout.getTabAt(3);
     }
 }
