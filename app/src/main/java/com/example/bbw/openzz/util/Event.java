0e82d8d15182cec3517a3171718f4aa2b73810bf
bingweibi <794654932@qq.com>
2017-10-31 21:33:08 +0800

    1031 显示数据返回，cardView存在问题

---
+++ app/src/main/java/com/example/bbw/openzz/util/Event.java
@@ -0,0 +1,19 @@
+package com.example.bbw.openzz.util;
+
+/**
+ * Created by bbw on 2017/10/31.
+ * @author bbw
+ */
+
+public class Event {
+
+    private String responseMessage;
+
+    public Event(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+}
